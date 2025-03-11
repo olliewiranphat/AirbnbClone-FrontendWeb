a2bc19a744327da4d51772d2ed14cc7a3ae2fa4a
olliewiranphat <oil.wiranphat@gmail.com>
2025-03-11 17:11:36 +0700

    Edit MainIcons Aligin

--- src/layout/PublicLayout.jsx
+++ src/layout/PublicLayout.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import Footer from '../components/Footer'
 import MainNavBar from '../components/home-page/main-navbar/MainNavBar'
-import MainIcons from '../components/home-page/MainIcons'
 
 function PublicLayout() {
     return (
         <>
             <div className='fixed w-full z-50'>
                 <MainNavBar />
-                <MainIcons />
+
             </div>
             <div className='pt-[20%] w-full'>
                 <Outlet />
