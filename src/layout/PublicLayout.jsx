c095f8971e022a7a137346095ac29a4fad1e4867
Nuiicode <167839319+wathanyuok@users.noreply.github.com>
2025-03-13 13:44:34 +0700

    update

--- src/layout/PublicLayout.jsx
+++ src/layout/PublicLayout.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import Footer from '../components/Footer'
 import MainNavBar from '../components/home-page/main-navbar/MainNavBar'
+import MainIcons from '../components/home-page/MainIcons'
 
 function PublicLayout() {
     return (
         <>
             <div className='fixed w-full z-50'>
                 <MainNavBar />
-
+                <MainIcons />
             </div>
             <div className='pt-[280px] w-full'>
                 <Outlet />
