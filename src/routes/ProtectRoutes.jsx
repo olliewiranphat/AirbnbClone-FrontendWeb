c7012e0254ac5b6eb6226ac3ba5d15beed6e3274
ChartMongkolsin <Chart_mongkolsin@hotmail.com>
2025-03-13 12:41:34 +0700

    admin

--- src/routes/ProtectRoutes.jsx
+++ src/routes/ProtectRoutes.jsx
@@ -4,33 +4,35 @@ import { Loader } from "lucide"
 
 function ProtectRoutes({ el, allows }) {
 
-    const { user } = useUser()
+    // const { user } = useUser()
     // console.log('user', user);
 
-    const role = user?.publicMetadata.role
+    // const role = user?.publicMetadata.role
+    const role = "ADMIN"
+    
     // console.log('role', role);
 
 
-    const { isSignedIn, isLoaded } = useAuth()
-    if (!isLoaded) {
-        return <Loader className='m-auto mt-[22%] animate-spin text-gray-400 font-semibold' />
-    }
+    // const { isSignedIn, isLoaded } = useAuth()
+    // if (!isLoaded) {
+    //     return <Loader className='m-auto mt-[22%] animate-spin text-gray-400 font-semibold' />
+    // }
 
-    if (!isSignedIn) {
-        return (
-            <SignInButton mode='madal'>
-            </SignInButton >
-        )
-    }
+    // if (!isSignedIn) {
+    //     return (
+    //         <SignInButton mode='madal'>
+    //         </SignInButton >
+    //     )
+    // }
 
-    if (!allows.includes(role)) {
-        return (
-            <div className='flex flex-col gap-2 m-auto mt-[20%]'>
-                <span className="font-semibold">Access Denied!!</span>
-                <Link to='/' className='py-2 px-4 rounded-md bg-[#FF385C] text-white hover:font-semibold hover:bg-[#dd1062]'>Go to Home</Link>
-            </div>
-        )
-    }
+    // if (!allows.includes(role)) {
+    //     return (
+    //         <div className='flex flex-col gap-2 m-auto mt-[20%]'>
+    //             <span className="font-semibold">Access Denied!!</span>
+    //             <Link to='/' className='py-2 px-4 rounded-md bg-[#FF385C] text-white hover:font-semibold hover:bg-[#dd1062]'>Go to Home</Link>
+    //         </div>
+    //     )
+    // }
 
 
     return el
