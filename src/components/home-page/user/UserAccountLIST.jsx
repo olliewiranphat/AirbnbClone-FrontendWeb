b287f86fa875f7a1e4f288e42aea0dd2f93f670e
Nuiicode <167839319+wathanyuok@users.noreply.github.com>
2025-03-19 15:52:41 +0700

    Update UserAccountLIST.jsx

--- src/components/home-page/user/UserAccountLIST.jsx
+++ src/components/home-page/user/UserAccountLIST.jsx
@@ -7,14 +7,14 @@ function UserAccountLIST() {
         <div className='absolute top-[50px] shadow-xl right-0 rounded-md flex items-start flex-col text-[14px] w-[250px] bg-white z-50'>
             <div className='w-full px-4 py-5 border-b-[1px] border-gray-300 flex flex-col gap-5 items-start'>
                 <ReloadLink to='/guest/messages' className='strong'>Message</ReloadLink>
-                <ReloadLink to='/notifications' className='strong'>Notifications</ReloadLink>
-                <ReloadLink to='/trips' className='strong'>Trips</ReloadLink>
+{/*                 <ReloadLink to='/notifications' className='strong'>Notifications</ReloadLink>
+                <ReloadLink to='/trips' className='strong'>Trips</ReloadLink> */}
                 <ReloadLink to='/wishlists' className='strong'>Wishlists</ReloadLink>
             </div>
             <div className='w-full px-4 py-5 border-b-[1px] border-gray-300 flex flex-col gap-5 items-start'>
                 <ReloadLink to='/host/homes'>Airbnb your home</ReloadLink>
-                <ReloadLink to='/host/experiences'>Host an experience</ReloadLink>
-                <ReloadLink to='/account-settings'>Account</ReloadLink>
+{/*                 <ReloadLink to='/host/experiences'>Host an experience</ReloadLink>
+                <ReloadLink to='/account-settings'>Account</ReloadLink> */}
             </div>
             <div className='px-4 py-5 flex flex-col gap-5 items-start'>
                 <ReloadLink to='/giftcards'>Gift cards</ReloadLink>
